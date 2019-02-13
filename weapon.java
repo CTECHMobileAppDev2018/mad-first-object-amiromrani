3ab02f365f3dc9bb52a4d933b983d1a9bc2bfe81
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-02-13 08:34:24 -0600

    Create weapon.java

---
+++ weapon.java
@@ -0,0 +1,16 @@
+public class Weapon  {
+  
+ private String type;
+ private int damage;
+  
+ public void setType(String inputType) {
+   if (input.type() != "Gun" ) {
+      this.type = "invalid";
+      return;
+   
+   
+ }
+ }
+ 
+ 
+}
