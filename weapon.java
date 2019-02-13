fc97c58695be2d33139e5a4c43c5af530f39aa25
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-02-13 08:48:04 -0600

    Update weapon.java

--- weapon.java
+++ weapon.java
@@ -1,15 +1,15 @@
-public class Weapon  {
+public class weapon  {
   
  private String type;
  private int damage;
   
- public void setType(String inputType) {
-   if (input.type() != "Gun" ) {
+ public void settype(String inputType) {
+   if (inputType != "Gun" ) {
       this.type = "invalid";
       return;
+   }
+   this.type = "Gun";
    
-   
- }
  }
  
  
