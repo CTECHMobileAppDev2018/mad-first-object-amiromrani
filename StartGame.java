b3195feff1f130cc0b8c8f5e9f8623fec647cbab
amiromrani <46926811+amiromrani@users.noreply.github.com>
2019-02-13 08:34:20 -0600

    Update StartGame.java

--- StartGame.java
+++ StartGame.java
@@ -8,8 +8,15 @@ public class StartGame {
     
     System.out.println("Player1 name is: " + player1.getName());
     
+    Player player2 = new Player();
+    player2.setName("amir");
+    
+    System.out.println("Player2 name is: " + player2.getName());
+    
     // this won't work until you create a getHealth method in your object. Uncomment it when you do.
-    // System.out.println(player1.getName() + "'s health is: " + player1.getHealth());
+    System.out.println(player1.getName() + "'s health is: " + player1.getHealth());
+    
+    
   }  
   
 }
